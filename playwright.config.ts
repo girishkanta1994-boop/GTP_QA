199a8fdf0d6613ce0658b9ac5c043164fef30fa8
sahanadevivaradharajan <sahanadevi.v@royalcyber.com>
2025-01-07 21:45:10 +0530

    with junit

--- playwright.config.ts
+++ playwright.config.ts
@@ -120,14 +120,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
+ use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+   trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
@@ -167,6 +167,10 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
+ reporter: [
+   ['list'], // Optional: Console output
+   ['junit', { outputFile: 'results/junit-results.xml' }]
+],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
